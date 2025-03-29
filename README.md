04d790920698d1a11de123eb961365ba7a04e37e
asamasadau <asamasadau@evolution.com>
2025-03-29 19:31:52 +0100

    WIP: Echo Server / One-Way tunnel

--- README.md
+++ README.md
@@ -8,6 +8,13 @@
 - My way to learn things is: cycle of practice and theory with gradual dive step by step, so I want to start with something as a first step
 - Found an artical 'bout low level tcp implementation on zig, so will follow it
 
+## 2025-03-29
+- I was playing around with low-level tcp server implementation and found more high-level during research of the std lib, so I decided to use it
+- The echo server was pretty straightforward to implement
+- I implemented a simple chain Client -> Proxy -> AMPQ server, it starts the handshake process, but the idea is to implement a tunnel, which is protocol agnostic
+
+_btw was pretty fun to find Zig compile time duck typing_
+
 
 ===
 - `zig build run`
--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,46 @@
 const std = @import("std");
-const net = std.net;
-const posix = std.posix;
 
 pub fn main() !void {
-    const address = try std.net.Address.parseIp("127.0.0.1", 4321);
+    var gpa_instance = std.heap.GeneralPurposeAllocator(.{}).init;
+    const allocator = gpa_instance.allocator();
 
-    const tpe: u32 = posix.SOCK.STREAM;
-    const protocol = posix.IPPROTO.TCP;
-    const listener = try posix.socket(address.any.family, tpe, protocol);
-    defer posix.close(listener);
+    const proxy_address = try std.net.Address.parseIp("127.0.0.1", 5672);
+    var server = try std.net.Address.listen(proxy_address, .{});
+    defer server.deinit();
+
+    var client = try server.accept();
+    defer client.stream.close();
+
+    const client_reader = client.stream.reader();
+    //const client_writer = client.stream.writer();
+
+    try start_handshake_with_rabbit(client_reader, allocator);
+}
+
+pub fn echo_response(client_reader: anytype, client_writer: anytype, allocator: std.mem.Allocator) !void {
+    while (true) {
+        const msg = try client_reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 65536) orelse break;
+        defer allocator.free(msg);
+
+        std.log.info("Recieved message From Client: \"{}\"", .{std.zig.fmtEscapes(msg)});
+
+        try client_writer.writeAll(msg);
+    }
+}
+
+pub fn start_handshake_with_rabbit(client_reader: anytype, allocator: std.mem.Allocator) !void {
+    var rabbit_stream = try std.net.tcpConnectToHost(allocator, "127.0.0.1", 5671);
+    defer rabbit_stream.close();
+    const rabbit_writer = rabbit_stream.writer();
+
+    var client_buf: [1024]u8 = undefined;
+
+    const n = try client_reader.read(&client_buf);
+    const slice = client_buf[0..n];
+    std.log.info("Slice:{s} ", .{slice});
+    rabbit_writer.writeAll(slice) catch |err| {
+        std.log.err("Failed to send to Rabbit", .{});
+        return err;
+    };
+    std.log.info("Connection closed", .{});
 }
